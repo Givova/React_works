73c20eee0f22208a488be6adac6a17c4f3c3510b
Kukushenok <gusarov.ml@mail.ru>
2025-03-23 14:42:22 +0300

    lesson 278

---
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -0,0 +1,17 @@
+import courses from '../data/courses'
+import {Link, useParams} from 'react-router-dom'
+const SingleCourse = () => {
+    const params = useParams()
+    const course = courses.find((course) => course.slug === params.slug)
+  return (
+    <>
+    <h1>{course.title}</h1>
+    <h2>{course.slug}</h2>
+    <h3>{course.id}</h3>
+    <Link to='..' relative='path'>All courses</Link>
+    
+    </>
+  )
+}
+
+export default SingleCourse
